b3a7abf12b53131606925a4e9609bd25570e2a68
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2020-12-13 21:17:46 +0100

    New Updates

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
-from .views import RoomView
+from .views import GetRoom, RoomView, CreateRoomView
 from django.urls import path
 
 urlpatterns = [
     path('home', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view())
 ]
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
 
 export default class HomePage extends Component {
@@ -13,10 +14,11 @@ export default class HomePage extends Component {
         <Router>
             <Switch>
                 <Route exact path='/'>
-                    <p>This is the new home page</p>
+                    <p>This is the new home page, where my strange JS router is</p>
                 </Route>
                 <Route path='/join' component={RoomJoinPage}/>
                 <Route path='/create' component={CreateRoomPage}/>
+                <Route path='/room/:roomCode' component={Room}/>
             </Switch>
         </Router>
         );
